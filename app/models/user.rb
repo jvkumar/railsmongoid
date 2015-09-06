744e13281e05a407bf71f641b1990a2c0f580f5b
Jai Kumar <jaivardhan@gmail.com>
2015-09-06 10:36:20 -0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User
 
   field :name,  type: String
   field :user_name,  type: String
-  field :status,   type: String, default: "active""avtar_image"
+  field :status,   type: String, default: "active"
   field :avatar_image,   type: String
 
   validates_presence_of :user_name
